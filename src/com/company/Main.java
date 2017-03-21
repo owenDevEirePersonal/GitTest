1a75d2fb845ffd7a2612242844c68ec5f1df4701
owenDevEirePersonal <owen@deveire.com>
2017-03-21 15:38:32 +0000

    Added printLogo method.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,8 +2,20 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 	    System.out.print("This better work.");
 	    System.out.print("\n\n\n\n\n\n\n\n\n");
+	    printLogo();
+    }
+
+    private static void printLogo()
+    {
+        System.out.print(" __    __   ______  ______\n" +
+                         "|  |  |  | |  ____| |  __  |\n" +
+                         "|  |__|  | | |____  | |__| |\n" +
+                         "|   __   | |  ____| |  __  |\n" +
+                         "|  |  |  | | |____  | |  | |\n" +
+                         "|__|  |__| |______| |_|  |_|");
     }
 }
