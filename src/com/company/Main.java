0c75c341c9ad507595aa8e898eb5440865bd01d7
owenDevEirePersonal <owen@deveire.com>
2017-03-21 15:23:14 +0000

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.print("This better work.");
+	    System.out.print("\n\n\n\n\n\n\n\n\n");
+    }
+}
